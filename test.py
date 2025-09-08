8e9de1757364c5db30c822359d5a4a5743851967
amberhandal <ambahjh@gmail.com>
2025-09-08 13:51:07 -0500

    output start pos and goal pos for debugging

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 from maze.maze import Maze
 
 def main():
-    print("Default Maze:")
+    print("LOAD DEFAULT MAZE:")
     default_maze = Maze.from_json('maze-templates.json', 'DEFAULT_MAZE')
     print(default_maze)
-    
-    # print("Test Maze:")
-    # test_maze = Maze.from_json('maze-templates.json', 'TEST_MAZE')
-    # print(test_maze)
+    print(f"Start pos: {default_maze.start_position}")
+    print(f"Goal pos: {default_maze.goal_position}")
 
 if __name__ == "__main__":
     main()
