7451ba3fde0615081e1d190b326b4c6804d8a58a
KThompson2002 <kylezthompson@gmail.com>
2025-09-08 17:09:07 -0500

    Organized class into belief.py

---
+++ belief.py
@@ -0,0 +1,122 @@
+import numpy as np
+
+class Belief_Status():
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.tot = 0
+        self.old_loc = np.empty((h, w), dtype=int)
+        self.loc = np.empty((h, w), dtype=int)
+
+    def set_locations(self, maze):
+        for i in range(self.h):
+            for j in range(self.w):
+                if maze[i][j] != "#":
+                    self.loc[i][j] = 1
+                    self.tot += 1
+                else:
+                    self.loc[i][j] = 0
+    
+    def new_move(self, maze, dir):
+        self.old_loc = self.loc.copy()
+        for i in range(self.h):
+            for j in range(self.w):
+                if self.loc[i][j] == 1 or self.loc[i][j] == 12:
+                    if self.check_viable(dir, maze, i, j):
+                        self.update_avail(dir, i, j)
+                    else:
+                        self.update_no(dir, i, j)
+        self.update_total()
+    
+    def update_total(self):
+        new_tot = 0
+        for i in range(self.h):
+            for j in range(self.w):
+                if self.loc[i][j] > 1:
+                    self.loc[i][j] = 1
+                    new_tot += 1
+                elif self.loc[i][j] == 1:
+                    new_tot += 1   
+        print(self.loc)
+        if new_tot >= self.tot:
+            self.loc = self.old_loc.copy()
+        else:
+            self.tot = new_tot
+
+    
+    def check_viable(self, dir, maze, x, y):
+        if dir == 0:
+            tmp = maze[x+1][y]
+            if tmp != '#':
+                return True
+            else:
+                return False
+        elif dir == 1:
+            tmp = maze[x][y+1]
+            if tmp != '#': 
+                return True
+            else:
+                return False
+        elif dir == 2:
+            tmp = maze[x-1][y]
+            if tmp != '#':
+                return True
+            else:
+                return False
+        else:
+            tmp = maze[x][y-1]
+            if tmp != '#': 
+                return True
+            else:
+                return False
+
+    def update_avail(self, dir, x, y):
+        if dir == 0:
+            if self.loc[x+1][y] == 1:
+                self.loc[x+1][y] = 12
+            else:
+                self.loc[x+1][y] = 2
+
+            if self.loc[x][y] <= 1:
+                self.loc[x][y] = 0
+            
+        elif dir == 1:
+            if self.loc[x][y+1] == 1:
+                self.loc[x][y+1] = 12
+            else:
+                self.loc[x][y+1] = 2
+                
+            if self.loc[x][y] <= 1:
+                self.loc[x][y] = 0
+        elif dir == 2:
+            if self.loc[x-1][y] == 1:
+                self.loc[x-1][y] = 12
+            else:
+                self.loc[x-1][y] = 2
+
+            if self.loc[x][y] <= 1:
+                self.loc[x][y] = 0
+        else:
+            if self.loc[x][y-1] == 1:
+                self.loc[x][y-1] = 12
+            else:
+                self.loc[x][y-1] = 2
+
+            if self.loc[x][y] <= 1:
+                self.loc[x][y] = 0
+
+    def update_no(self, dir, x, y):
+        if dir == 0:
+            self.loc[x+1][y] = 0
+        elif dir == 1:
+            self.loc[x][y+1] = 0
+        elif dir == 2:
+            self.loc[x-1][y] = 0
+        else:
+            self.loc[x][y-1] = 0
+
+    def final_loc(self):
+        for i in range(self.h):
+            for j in range(self.w):
+                if self.loc[i][j] == 1:
+                    return np.array([i, j])
