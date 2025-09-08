12e0634344ac48379eb2315d7352efdc0cec57d5
KThompson2002 <kylezthompson@gmail.com>
2025-09-08 10:59:03 -0500

    Updated test-maze.txt to be more readable by loop Created local robot and stored goal and start

--- belief-state-planner.py
+++ belief-state-planner.py
@@ -1,7 +1,41 @@
 #!usr/bin/env python3
 import numpy as np
+from robot import Robot
+# import Maze
+
+maze_name = "test-maze.txt"
+
+# def belief():
 
 def main():
+    w = 6
+    h = 4
+    goal = np.array([0, 0])
+    start = np.array([0, 0]) 
+    maze = np.empty((h, w), dtype=str)
+    with open(maze_name) as f:
+        i = 0
+        for x in f:
+            for j in range(len(x)-1):
+                maze[i][j] = x[j]
+                if (x[j] == 'R'):
+                    start[0] = i
+                    start[1] = j
+                if (x[j] == 'G'):
+                    goal[0] = i
+                    goal[1] = j
+            i += 1
+
+    for line in maze:
+        for char in line:
+            print(char, end=" ")
+        print()
+
+    lil_guy = Robot(start[0], start[1], False)
+
+    # while true:
+        
+
 
 if __name__ == "__main__":
     main()
