98bd702a2fd6c75d92c94f9267a1a5f2ce859b2b
amberhandal <ambahjh@gmail.com>
2025-09-08 14:03:38 -0500

    file cleanup

--- maze.py
+++
@@ -1,2 +0,0 @@
-# manual maze generation for robot simulation using ascii art
-# "#" represents walls, " " represents non-wall spaces, "R" represents the robot's starting position, "G" represents the goal position
