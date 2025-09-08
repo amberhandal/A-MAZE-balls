fed9bb2300d019c79d464110b4d5296909cb575e
gaiosa <gregory.aiosa@gmail.com>
2025-09-08 15:54:24 -0500

    Implemented the recursive backtracking and got it to work with a simple maze.

--- rec_back_solve.py
+++ rec_back_solve.py
@@ -8,19 +8,47 @@ from robot import Robot
 
 # from pathlib import Path
 
-def open_maze():
-    """Pulls in the maze file and finds the start."""
-    maze = np.array([['#','#','#','#','#','#'],
+maze = np.array([['#','#','#','#','#','#'],
                     ['#','R','#','#','#','#'],
                     ['#',' ',' ',' ','G','#'],
                     ['#','#','#','#','#','#']])
-    # print(maze)
+
+# Constants used in this program:
+WALL: str = '#'
+EMPTY: str = ' '
+START: str = 'R'
+GOAL: str = 'G'
+PATH: str = '.'
+
+# Get the height and width of the maze and find start location
+HEIGHT = len(maze)
+WIDTH = 0
+for row in maze:
+    if len(row) > WIDTH:
+        WIDTH = len(row)
+
+# Make each row in the maze a list as wide as the width
+for ii in range(len(maze)):
+    maze[ii] = list(maze[ii])
+    if len(maze[ii]) != WIDTH:
+        maze[ii] = [EMPTY] * WIDTH
+
+def print_maze(maze):
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(maze[y][x], end='')
+        print()
+    print()
+
+def find_start(maze):
+    """Pulls in the maze file and finds the start."""
+
+    # Find the start position of the robot
     xx = 0
     for row in maze:
         yy = 0
         for element in row:
-            #print(f"({xx},{yy}) ")
-            if element == 'R':
+            if element == START:
                 x_start, y_start = xx,yy
             yy +=1
         xx += 1
@@ -28,14 +56,53 @@ def open_maze():
     print(f"Robot starting position: ({x_start},{y_start})")
     return x_start, y_start
 
-def find_next_move(x, y):
-    """Finds the next best spot to move based on recursive backtracking."""
+def solve_maze(maze, x=None, y=None, visited=None):
+    """Solves the maze using recursive backtracking"""
+    if x == None or y == None:
+        x, y = find_start(maze)
+        maze[y][x] = EMPTY 
+    if visited == None:
+        visited = []
+
+    if maze[y][x] == GOAL:
+        return True
+
+    maze[y][x] = PATH
+    visited.append(str(x) + ',' + str(y))
+
+    # Check the north position
+    if y + 1 < HEIGHT and maze[y + 1][x] in (EMPTY, GOAL) and \
+    str(x) + ',' + str(y + 1) not in visited:
+        if solve_maze(maze, x, y + 1, visited):
+            return True
+    # Check the south position
+    if y - 1 >= 0 and maze[y - 1][x] in (EMPTY, GOAL) and \
+    str(x) + ',' + str(y - 1) not in visited:
+        if solve_maze(maze, x, y - 1, visited):
+            return True
+    # Check the east position
+    if x + 1 < WIDTH and maze[y][x + 1] in (EMPTY, GOAL) and \
+    str(x + 1) + ',' + str(y) not in visited:
+        if solve_maze(maze, x + 1,y, visited):
+            return True
+    # Check the west position
+    if x - 1 >= 0 and maze[y][x - 1] in (EMPTY, GOAL) and \
+    str(x - 1) + ',' + str(y) not in visited:
+        if solve_maze(maze, x - 1, y, visited):
+            return True
+        
+    maze[y][x] = EMPTY
+
+    return False
 
 def main():
     """Initiates the solving algorithm."""
-    x_start, y_start = open_maze()
-    robotinator3000 = Robot(x_start,y_start, False)
-    print(robotinator3000.x)
+    #x_start, y_start = find_start()
+    #robotinator3000 = Robot(x_start,y_start, False)
+    #print(robotinator3000.x)
+    print_maze(maze)
+    solve_maze(maze)
+    print_maze(maze)
 
 if __name__ == "__main__":
     main()
