3e0cc5dff9c8b5c2e02a3255fd20081c0b05a8bb
KThompson2002 <kylezthompson@gmail.com>
2025-09-08 17:09:59 -0500

    Moved Belief_Status class to a new file Implemented working algorithm on simple mazes. Fails on larger mazes.

--- belief.py
+++ belief.py
@@ -5,9 +5,13 @@ class Belief_Status():
         self.w = w
         self.h = h
         self.tot = 0
+<<<<<<< HEAD
         self.old_loc = np.empty((h, w), dtype=int)
         self.loc = np.empty((h, w), dtype=int)
 
+=======
+        self.loc = np.empty((h, w), dtype=int)
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
     def set_locations(self, maze):
         for i in range(self.h):
             for j in range(self.w):
@@ -18,7 +22,11 @@ class Belief_Status():
                     self.loc[i][j] = 0
     
     def new_move(self, maze, dir):
+<<<<<<< HEAD
         self.old_loc = self.loc.copy()
+=======
+        
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
         for i in range(self.h):
             for j in range(self.w):
                 if self.loc[i][j] == 1 or self.loc[i][j] == 12:
@@ -29,11 +37,16 @@ class Belief_Status():
         self.update_total()
     
     def update_total(self):
+<<<<<<< HEAD
         new_tot = 0
+=======
+        self.tot = 0
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
         for i in range(self.h):
             for j in range(self.w):
                 if self.loc[i][j] > 1:
                     self.loc[i][j] = 1
+<<<<<<< HEAD
                     new_tot += 1
                 elif self.loc[i][j] == 1:
                     new_tot += 1   
@@ -43,6 +56,12 @@ class Belief_Status():
         else:
             self.tot = new_tot
 
+=======
+                    self.tot += 1
+                elif self.loc[i][j] == 1:
+                    self.tot += 1   
+        print(self.loc)     
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
     
     def check_viable(self, dir, maze, x, y):
         if dir == 0:
@@ -77,7 +96,11 @@ class Belief_Status():
             else:
                 self.loc[x+1][y] = 2
 
+<<<<<<< HEAD
             if self.loc[x][y] <= 1:
+=======
+            if (self.loc[x][y] <= 1):
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
                 self.loc[x][y] = 0
             
         elif dir == 1:
@@ -86,7 +109,11 @@ class Belief_Status():
             else:
                 self.loc[x][y+1] = 2
                 
+<<<<<<< HEAD
             if self.loc[x][y] <= 1:
+=======
+            if (self.loc[x][y] <= 1):
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
                 self.loc[x][y] = 0
         elif dir == 2:
             if self.loc[x-1][y] == 1:
@@ -94,7 +121,11 @@ class Belief_Status():
             else:
                 self.loc[x-1][y] = 2
 
+<<<<<<< HEAD
             if self.loc[x][y] <= 1:
+=======
+            if (self.loc[x][y] <= 1):
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
                 self.loc[x][y] = 0
         else:
             if self.loc[x][y-1] == 1:
@@ -102,7 +133,11 @@ class Belief_Status():
             else:
                 self.loc[x][y-1] = 2
 
+<<<<<<< HEAD
             if self.loc[x][y] <= 1:
+=======
+            if (self.loc[x][y] <= 1):
+>>>>>>> 2fb9655 (Moved Belief_Status class to a new file)
                 self.loc[x][y] = 0
 
     def update_no(self, dir, x, y):
