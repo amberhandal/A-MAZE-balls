48ed01adca3ff9e670c7d1cbab0e7d92c9b0721e
amberhandal <ambahjh@gmail.com>
2025-09-08 09:32:00 -0500

    maze format planning

---
+++ maze.py
@@ -0,0 +1,2 @@
+# manual maze generation for robot simulation using ascii art
+# "#" represents walls, " " represents non-wall spaces, "R" represents the robot's starting position, "G" represents the goal position
